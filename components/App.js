2ce8a54b7447d955644647ad2b4c9534f833f926
Bartłomiej Bajda <allerbbb@gmail.com>
2018-05-31 17:19:01 +0000

    add project

---
+++ components/App.js
@@ -0,0 +1,69 @@
+var App = React.createClass({
+  getInitialState() {
+    return {
+      loading: false,
+      searchingText: '',
+      gif: {}
+    };
+  },
+	
+  handleSearch: function(searchingText) {
+    this.setState({
+      loading: true
+    });
+    this.getGif(searchingText).then (gif => {
+      this.setState({
+        loading: false,
+        gif: gif,
+        searchingText: searchingText
+      });
+    });
+  },
+  
+  getGif: function(searchingText) {
+    return new Promise (
+      (resolve, reject) => {
+        var url = 'https://api.giphy.com' + '/v1/gifs/random?api_key=' + '79d4ejI5WnmHsZqbA6TZOyeG2iB4101D' + '&tag=' + searchingText;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function() {
+          if (xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText).data;
+            var gif = {
+              url: data.fixed_width_downsampled_url,
+              sourceUrl: data.url
+            };
+            resolve(gif);
+      }    
+      reject(new Error(this.statusText));   
+      }
+      xhr.onerror = function () {
+        reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+      };
+    xhr.send();
+    }
+  );
+  },
+	
+  render: function() {
+
+    var styles = {
+      margin: '0 auto',
+      textAlign: 'center',
+      width: '90%'
+    };
+
+    return (
+      <div style={styles}>
+        <h1>Wyszukiwarka GIFow!</h1>
+        <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+        <Search onSearch={this.handleSearch}/>
+        <Gif 
+          loading={this.state.loading}
+          url={this.state.gif.url}
+          sourceUrl={this.state.gif.sourceUrl}
+        />
+      </div>
+    );
+  }
+});
